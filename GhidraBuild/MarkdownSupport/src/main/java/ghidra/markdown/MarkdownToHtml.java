c540095d08f90f773982efc0cd3c32cf6e6bb64a
Ryan Kurtz <ryanmkurtz@users.noreply.github.com>
2024-10-04 10:24:17 -0400

    links

--- GhidraBuild/MarkdownSupport/src/main/java/ghidra/markdown/MarkdownToHtml.java
+++ GhidraBuild/MarkdownSupport/src/main/java/ghidra/markdown/MarkdownToHtml.java
@@ -2,12 +2,14 @@ package ghidra.markdown;
 
 import java.io.*;
 import java.util.List;
+import java.util.Map;
 
 import org.commonmark.Extension;
 import org.commonmark.ext.heading.anchor.HeadingAnchorExtension;
+import org.commonmark.node.Link;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
-import org.commonmark.renderer.html.HtmlRenderer;
+import org.commonmark.renderer.html.*;
 
 public class MarkdownToHtml {
 
@@ -30,12 +32,37 @@ public class MarkdownToHtml {
 
 		List<Extension> extensions = List.of(HeadingAnchorExtension.create());
 		Parser parser = Parser.builder().extensions(extensions).build();
-		HtmlRenderer renderer = HtmlRenderer.builder().extensions(extensions).build();
+		HtmlRenderer renderer = HtmlRenderer.builder()
+				.extensions(extensions)
+				.attributeProviderFactory(new LinkAttributeProvider())
+				.build();
 		Node document = parser.parseReader(new FileReader(inFile));
+		
 		String html = renderer.render(document);
-
 		try (PrintWriter out = new PrintWriter(outFile)) {
 			out.write(html);
+		}
+	}
+	
+	/**
+	 * Class to help adjust links to Markdown files to instead become links to HTML files
+	 */
+	private static class LinkAttributeProvider
+			implements AttributeProvider, AttributeProviderFactory {
+
+		@Override
+		public AttributeProvider create(AttributeProviderContext attributeProviderContext) {
+			return new LinkAttributeProvider();
+		}
+		
+		@Override
+		public void setAttributes(Node node, String tagName, Map<String, String> attributes) {
+			if (node instanceof Link) {
+				String href = attributes.get("href");
+				if (href != null && !href.startsWith("#") && href.toLowerCase().endsWith(".md")) {
+					attributes.put("href", href.substring(0, href.length() - 2) + "html");
+				}
+			}
 		}
 	}
 }

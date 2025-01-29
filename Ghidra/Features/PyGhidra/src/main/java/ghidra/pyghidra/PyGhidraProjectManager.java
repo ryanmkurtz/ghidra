9c38ba72d2428a02909ec78c5ec415f73da23daa
Ryan Kurtz <ryanmkurtz@users.noreply.github.com>
2025-01-29 11:31:30 -0500

    New PyGhidra API

---
+++ Ghidra/Features/PyGhidra/src/main/java/ghidra/pyghidra/PyGhidraProjectManager.java
@@ -0,0 +1,10 @@
+package ghidra.pyghidra;
+
+import ghidra.framework.project.DefaultProjectManager;
+
+/**
+ * A {@link DefaultProjectManager} for use by PyGhidra
+ */
+public class PyGhidraProjectManager extends DefaultProjectManager {
+	// This class exists just to allow access to the proteted constructor
+}
